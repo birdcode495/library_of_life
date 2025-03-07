5d44562e1653c4e7a3e56f8912dbb2dba0771f1c
BirdCode888 <birdcode.cali@gmail.com>
2025-03-07 14:27:10 -0500

    Creation of the report and the breeding season timer method for the Milvus migrans class.

--- BirdCode_Classes.py
+++ BirdCode_Classes.py
@@ -12,6 +12,7 @@
 # -------------------------------------------------------------------------------------------------------------------------------
 
 
+import datetime
 
 class Milvus_migrans():
 
@@ -33,3 +34,57 @@ class Milvus_migrans():
 	mean_weight_g = 875
 	movement_patterns = 'Full migrant'
 	ability_to_fly = True
+	breeding_season_start = 1
+	breeding_season_finish = 2
+
+	def breeding_timer(self):
+
+		x = datetime.datetime.now()
+		
+		if self.breeding_season_start < self.breeding_season_finish:
+
+			if x.month < self.breeding_season_start or x.month > self.breeding_season_finish:
+
+				print('Now is not breeding season for this species')
+
+			else:
+
+				print('Now is the breeding season for this species ')
+
+
+		elif self.breeding_season_start > self.breeding_season_finish:
+
+			if x.month >= self.breeding_season_start:
+
+				print('Now is the breeding season for this species')
+
+			elif x.month <= self.breeding_season_finish:
+
+				print('Now is the breeding season for this species')
+
+			else:
+
+				print('Now is not breeding season for this species')
+
+
+
+
+# -------------------------------- Creation of this first object for this species --------------------------
+
+MilvusMigrans1 = Milvus_migrans()
+
+
+# -------------------------------- Creation of the initial report for the generated object ------------------
+
+
+print('--------------------------------------- SPECIES REPORT -----------------------------------------------')
+
+print()
+print()
+print('The scientific name of this species is: ', MilvusMigrans1.scientific_name)
+print('The english name for this species is: ', MilvusMigrans1.english_name)
+print('The chinese name for this species is: ', MilvusMigrans1.chinese_name)
+print('This species movement patterns are: ', MilvusMigrans1.movement_patterns)
+print()
+MilvusMigrans1.breeding_timer()
+
