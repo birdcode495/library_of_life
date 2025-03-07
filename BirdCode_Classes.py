08ed5964522d4c91e4afca8712031a4d9d813166
BirdCode888 <birdcode.cali@gmail.com>
2025-03-07 13:57:07 -0500

    Start of the BirdCode Project - Creation of initial properties for the Milvus migrans class

---
+++ BirdCode_Classes.py
@@ -0,0 +1,35 @@
+
+
+# -------------------------------------------------------------------------------------------------------------------------------
+
+
+#                                                     LIBRARY OF LIFE
+
+#                                            Construction of BirdCode Classes
+
+
+
+# -------------------------------------------------------------------------------------------------------------------------------
+
+
+
+class Milvus_migrans():
+
+	scientific_name = 'Milvus migrans'
+	english_name = 'Black kite'
+	chinese_name = '黑鸢'
+	kingdom = 'Animalia'
+	phylum = 'Chordata'
+	classs = 'Aves'
+	order = 'Accipitriformes'
+	family = 'Accipitridae'
+	genus = 'Milvus'
+	mean_number_mature_individuals = 4800000
+	global_assessment = 'LC'
+	lower_elevation_limit = 0
+	upper_elevation_limit = 4900
+	size_cm = 60
+	wingspan_cm = 150
+	mean_weight_g = 875
+	movement_patterns = 'Full migrant'
+	ability_to_fly = True
